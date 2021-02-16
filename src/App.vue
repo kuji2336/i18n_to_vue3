5ed31af9d1026d7742662c0348b4b6a733ff9732
kujx <quji.bolqvadze123@gmail.com>
2021-02-16 12:37:04 +0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="select-section">
+      <select name="cars" id="cars" @change="getVal($event.target.value)">
+        <option value="en">ENG</option>
+        <option value="ge">GEO</option>
+        <option value="ru">RUS</option>
+      </select>
+    </div>
+    <div class="text">
+       <p>{{t('title', {}, {locale:lang})}}</p>
+       <p>{{t('sub-text', {}, {locale:lang})}}</p>
+    </div>
+    <button>clcik</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {useI18n} from 'vue-i18n'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  setup(){
+   const {t,locale} = useI18n();
+   return {t,locale}
+  },
+  data(){
+   return{
+     lang:'ENG'
+   }
+  },
+   methods:{
+      getVal(lang){
+      this.lang = lang
+      console.log(this.lang)
+   }
+   }
+};
 </script>
 
 <style>
@@ -22,5 +46,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container{
+  max-width: 1300px;
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import geo from "./translations/geo.json"
+import eng from "./translations/eng.json"
+import rus from "./translations/rus.json"
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    messages:{
+        ge:geo,
+        en:eng,
+        ru:rus
+    },
+    fallbackLocale:'en'
+})
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
